430a269306aa53aaca3458e0672dd6d052e05092
Emerson palacio <emersonpalaciootalvaro@gmail.com>
2020-12-26 09:23:11 -0500

    firtsCommin

---
+++ MenuHamburguesa.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MenuHamburguesa.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
